fc2a61f1d2ace852a5eb6b11210deccec4fb40a7
Arpanel Franklin <arpanelfranklin@Arpanels-MacBook-Air.local>
2026-03-12 12:17:57 +0530

    Added backend

--- env/testing/backend.tf
+++ env/testing/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  
+  backend "s3" {
+    name = "${var.env}-s3-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "${var.env}-dynamodb-table"
+  }
 }
